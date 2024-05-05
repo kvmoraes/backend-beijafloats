dd7e7efb5c56767f7bf6491a06e41adc10a03ddc
froes <bkcauafroes2012@gmail.com>
2024-05-04 23:43:31 -0300

    crud base

--- src/server.js
+++ src/server.js
@@ -1,28 +1,33 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
-const healthchecker = require('./api/routes/healthcheck');
+const healthchecker = require("./api/routes/healthcheck");
+const routes = require("./api/routes/routes");
 
 const app = express();
 const PORT = 3000;
 
-app.use(cors({
-	origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
+// app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use('/', (req, res) => {
-	const message = "hello";
-	try {
-		res.status(200).send(message);
-	} catch (error) {
-		message = error;
-		res.status(400).send();
-	}
+app.use("/test", (req, res) => {
+  const message = "hello";
+  try {
+    res.status(200).send(message);
+  } catch (error) {
+    message = error;
+    res.status(400).send();
+  }
 });
-app.use('/api', healthchecker);
+
+app.use("/api", healthchecker);
+app.use("/api/v1", routes);
 
 app.listen(PORT, () => {
-	console.log(`🚀 Server is up and running on port:${PORT}`);
+  console.log(`🚀 Server is up and running on port:${PORT}`);
 });
